79df71612887b56373bd7fe8663d143d15d57111
HypnosCicero <1345764659@qq.com>
2024-06-21 07:43:49 +0800

    feat(myDynamicNN):change the step function

--- src/model/myDynamicNN.py
+++ src/model/myDynamicNN.py
@@ -5,22 +5,13 @@ import torch.nn.functional as F
 import random
 
 class CustomNeuron:
-    def __init__(self, input_size, index:int):
+    def __init__(self, input_size, index:int) -> None:
         self.weights = torch.randn(input_size, requires_grad=True) # 树突 TODO need to dynamic
         self.bias = torch.randn(1, requires_grad=True)
         self.lifetime = random.randint(50, 100)  # 随机设定一个生命周期
         self.index = index
         self.age = 0
 
-    def forward(self, x):
-        return x @ self.weights + self.bias
-
-    def step(self):
-        self.age += 1
-        if self.age > self.lifetime:
-            return False  # 返回False表示该神经元已经死亡
-        return True
-    
     def destroy(self) -> int :
         return self.index
     
@@ -32,4 +23,13 @@ class CustomNeuron:
         self.age += 2
         newNeuron.age = self.age
         return newNeuron
+
+    def forward(self, x):
+        return x @ self.weights + self.bias
+
+    def step(self) -> Intger:
+        self.age += 1
+        if self.age > self.lifetime:
+            return self.destroy() 
+        return -1 # -1 is meaning its alive
     
